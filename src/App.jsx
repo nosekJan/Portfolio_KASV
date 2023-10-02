28ea10f7b5d6d7c8cdd5405bf57145c6694f5826
janNosek <janosek1628@gmail.com>
2023-10-02 18:11:38 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function App() {
+
+  return (
+    <>
+      <div className="text-center text-5xl">
+          hello
+      </div>
+    </>
+  )
+}
+
+export default App
