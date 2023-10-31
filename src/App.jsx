e1c95ca3532699ca853d35cfc7216ea05e84b024
janNosek <janosek1628@gmail.com>
2023-10-31 16:44:17 +0100

    working on preload page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,30 @@
 import {BrowserRouter} from "react-router-dom";
 import {About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas} from './components';
+import {useEffect, useState} from "react";
+import ClipLoader from "react-spinners/CircleLoader";
 
 const  App = () => {
+    const [loading, setLoading] = useState(false);
+    useEffect(() => {
+        setLoading(true);
+        setTimeout(() => {
+            setLoading(false)
+        }, 5000);
+    }, []);
+
     return (
         <BrowserRouter>
+
+            {
+                loading?
+                    <ClipLoader
+                        className="bg-black min-h-screen min-w-full justify-center items-center flex"
+                        color={"#6D28D9"}
+                        loading={loading}
+                        size={250}
+                        aria-label="Loading Spinner"
+                        data-testid="loader"
+                    /> :
             <div className="relative z-0 bg-black">
                 <div className="bg-hero-pattern bg-cover min-h-screen">
                     <div>
@@ -21,6 +42,7 @@ const  App = () => {
                     <Contact/>
                 </div>
             </div>
+            }
         </BrowserRouter>
     )
 }
